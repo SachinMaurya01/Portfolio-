214975da1ab93d06ddcf0b25ca909f7f291e764a
Sachin Maurya <100995198+SachinMaurya01@users.noreply.github.com>
2022-10-29 11:46:16 +0530

    Update Skills.jsx

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -7,8 +7,8 @@ import ReactImg from '../assets/react.png';
 import Node from '../assets/node.png';
 import FireBase from '../assets/firebase.png';
 import GitHub from '../assets/github.png';
-import Tailwind from '../assets/tailwind.png';
-import Mongo from '../assets/mongo.png';
+// import Tailwind from '../assets/tailwind.png';
+// import Mongo from '../assets/mongo.png';
 
 const Skills = () => {
   return (
@@ -45,14 +45,14 @@ const Skills = () => {
                   <img className='w-20 mx-auto' src={Node} alt="HTML icon" />
                   <p className='my-4'>NODE JS</p>
               </div>
-              <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
-                  <img className='w-20 mx-auto' src={Mongo} alt="HTML icon" />
-                  <p className='my-4'>MONGO DB</p>
-              </div>
-              <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
-                  <img className='w-20 mx-auto' src={Tailwind} alt="HTML icon" />
-                  <p className='my-4'>TAILWIND</p>
-              </div>
+//               <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+//                   <img className='w-20 mx-auto' src={Mongo} alt="HTML icon" />
+//                   <p className='my-4'>MONGO DB</p>
+//               </div>
+//               <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+//                   <img className='w-20 mx-auto' src={Tailwind} alt="HTML icon" />
+//                   <p className='my-4'>TAILWIND</p>
+//               </div>
               <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
                   <img className='w-20 mx-auto' src={FireBase} alt="HTML icon" />
                   <p className='my-4'>FIREBASE</p>
